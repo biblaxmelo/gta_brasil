5129a5f9fa10d5303052007a171a0b8df20e1878
lucas silva <lucas.melo@liferay.com>
2021-05-11 15:11:46 -0300

    .sh sobre tudo

---
+++ exemplo1.sh
@@ -0,0 +1,18 @@
+#!/bin/SHELL_ATUAL
+#este e um cometario 
+#este e outro comentario
+   echo "este script contem comentarios."
+echo "seu nome de ususario e: $(whoami)"
+echo "info de hora atual e tempo que o computador esta ligado: "$(uptime)
+echo "o script esta executando do diretorio: $(pwd)"
+echo "a data atual e: "`date`
+site=www.lucas_desenrolado.com.br
+meu_numero_favorito=17
+_cidade="Recife"
+_bairro="Santo Amaro"
+_idade=22
+echo "um otimo site pra voce aprender a programar e se manter atualizado e: $site"
+echo "meu numero favorito e: $meu_numero_favorito"
+echo "minha cidade natal e: $_cidade"
+echo "meu bairro e: $_bairro"
+echo "minha idade e: $_idade"
